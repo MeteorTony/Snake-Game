d7688b222cb0ce1273907b753c900c0f1315763b
MeteorTony <meteormeteor1028@gmail.com>
2022-01-04 14:32:27 +0800

    game design completed

---
+++ grid.js
@@ -0,0 +1,18 @@
+const GRID_SIZE = 25;
+
+export function randomGridPosition() {
+  return {
+    x: Math.floor(Math.random() * GRID_SIZE) + 1,
+    y: Math.floor(Math.random() * GRID_SIZE) + 1,
+  };
+}
+
+export function outsideGrid(position) {
+  // CSS grids start from 1 (0 is outside)
+  return (
+    position.x < 1 ||
+    position.x > GRID_SIZE ||
+    position.y < 1 ||
+    position.y > GRID_SIZE
+  );
+}
---
+++ script.js
@@ -0,0 +1,43 @@
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
+import { update as updateFood, draw as drawFood } from './food.js'
+import { outsideGrid } from './grid.js'
+
+let lastRenderTime = 0;
+let gameOver = false;
+const gameBoard = document.getElementById("game-board");
+
+function main(currentTime) {
+  if (gameOver) {
+    if (confirm("You lost. Press ok to restart.")) {        // refresh page
+      window.location = "/";
+    }
+    return;     // terminate game
+  }
+
+  window.requestAnimationFrame(main); // game loop
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000; // convert to seconds
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return; // so update would be approximately 1 / snake speed
+
+  lastRenderTime = currentTime;
+
+  update();
+  draw();
+}
+
+window.requestAnimationFrame(main);
+
+function update() {
+  updateSnake();
+  updateFood();
+  checkDeath();
+}
+
+function draw() {
+  gameBoard.innerHTML = "";     // clear previous content
+  drawSnake(gameBoard);
+  drawFood(gameBoard);
+}
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
+}
